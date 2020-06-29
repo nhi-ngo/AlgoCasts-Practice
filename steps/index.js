c4774f17e5168a2ea7092e7b55dfe4c615520cfe
nhi-ngo <nhingo.y@gmail.com>
2020-06-29 14:30:12 -0400

    steps-recursion

--- steps/index.js
+++ steps/index.js
@@ -17,19 +17,41 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  for (let row = 0; row < n; row++) {
-    let stair = '';
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
 
-    for (let column = 0; column < n; column++) {
-      if (column <= row) {
-        stair += '#';
-      } else {
-        stair += ' ';
-      }
-    }
-    console.log(stair);
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
+
+// Recursion
+
+function steps(n, row = 0, stair = '') {
+  // Define the base cases
+  if (n === row) {
+    return;
   }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+
+  steps(n, row, stair);
 }
 
 module.exports = steps;
