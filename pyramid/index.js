3721ea56b0198d722fbf2e49627e8ed2c9018155
nhi-ngo <nhingo.y@gmail.com>
2020-07-01 13:41:42 -0400

    pyramid-recursion

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,22 +14,46 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-  const midpoint = Math.floor((n * 2 - 1) / 2);
+// function pyramid(n) {
+//   const midpoint = Math.floor((n * 2 - 1) / 2);
 
-  for (let row = 0; row < n; row++) {
-    let level = '';
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
 
-    for (let column = 0; column < n * 2 - 1; column++) {
-      if (midpoint - row <= column && midpoint + row >= column) {
-        level += '#';
-      } else {
-        level += ' ';
-      }
-    }
+//     for (let column = 0; column < n * 2 - 1; column++) {
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
 
-    console.log(level);
+//     console.log(level);
+//   }
+// }
+
+// Recursion
+
+function pyramid(n, row = 0, level = '') {
+  if (n === row) {
+    return;
   }
+
+  if (level.length === n * 2 - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midpoint = Math.floor((n * 2 - 1) / 2);
+  let add;
+
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+
+  pyramid(n, row, level + add);
 }
 
 module.exports = pyramid;
