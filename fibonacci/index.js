db1045b9951db6c98af6c34088374f2c38c8ea80
nhi-ngo <nhingo.y@gmail.com>
2020-07-18 15:11:39 -0400

    recursive solution

--- fibonacci/index.js
+++ fibonacci/index.js
@@ -23,10 +23,29 @@
 
 // 2. Recursion
 
+function memoize(fn) {
+  const cache = {};
+
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
 function fib(n) {
-  if (n < 2) return n;
+  if (n < 2) {
+    return n;
+  }
 
   return fib(n - 1) + fib(n - 2);
 }
+
+fib = memoize(fib);
 
 module.exports = fib;
