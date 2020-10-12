b82fec53dbc2d6f79194abd153ff3b8f254cae5f
nhi-ngo <nhingo.y@gmail.com>
2020-10-11 21:12:14 -0400

    sorting with selectionSort

--- sorting/index.js
+++ sorting/index.js
@@ -20,7 +20,19 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
 
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    swapNumbers(arr, i, minIndex)
+  }
+
+  return arr;
 }
 
 function mergeSort(arr) {
